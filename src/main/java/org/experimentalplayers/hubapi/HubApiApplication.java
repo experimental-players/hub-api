7e452c5e752f138058e445a4dfbdcb168a5e5ff0
excale <dev_excale@hotmail.com>
2021-08-01 21:47:23 +0200

    Init commit

---
+++ src/main/java/org/experimentalplayers/hubapi/HubApiApplication.java
@@ -0,0 +1,13 @@
+package org.experimentalplayers.hubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/experimentalplayers/hubapi/HubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.experimentalplayers.hubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
