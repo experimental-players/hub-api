fdb491a4a0cdabbf0613cbe3ec4e9e62dcaa2bed
excale <dev_excale@hotmail.com>
2021-08-07 01:39:15 +0200

    Edited some javadoc to a more detailed one

--- src/main/java/org/experimentalplayers/hubapi/config/ApplicationMappings.java
+++ src/main/java/org/experimentalplayers/hubapi/config/ApplicationMappings.java
@@ -1,10 +1,16 @@
 package org.experimentalplayers.hubapi.config;
 
 /**
- * Endpoint mappings listing for easy access
+ * Mappings for {@link org.experimentalplayers.hubapi.controllers.ApplicationController ApplicationController},
+ * listed in this class as constants for easy and detailed access.
  */
 public abstract class ApplicationMappings {
 
+	/**
+	 * The root location to access the {@link org.experimentalplayers.hubapi.models.ApplicationModel Application} API.
+	 *
+	 * @see org.experimentalplayers.hubapi.controllers.ApplicationController ApplicationController
+	 */
 	public static final String ROOT = "/application";
 
 	public static final String FIND_ALL = "/findAll";
--- src/main/java/org/experimentalplayers/hubapi/config/ProjectMappings.java
+++ src/main/java/org/experimentalplayers/hubapi/config/ProjectMappings.java
@@ -1,7 +1,16 @@
 package org.experimentalplayers.hubapi.config;
 
-public class ProjectMappings {
+/**
+ * Mappings for {@link org.experimentalplayers.hubapi.controllers.ProjectController ProjectController},
+ * listed in this class as constants for easy and detailed access.
+ */
+public abstract class ProjectMappings {
 
+	/**
+	 * The root location to access the {@link org.experimentalplayers.hubapi.models.ProjectModel Project} API.
+	 *
+	 * @see org.experimentalplayers.hubapi.controllers.ProjectController ProjectController
+	 */
 	public static final String ROOT = "/project";
 
 	public static final String FIND_ALL = "/findAll";
