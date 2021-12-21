04b2b02aa332d46ec1e04ab1da4f50b768a0266a
Emanuele Scaccia <dev_excale@hotmail.com>
2021-12-21 16:44:00 +0100

    Cleaned PageUtil, added JetBrains annotations

--- src/main/java/Utils/PageUtil.java
+++ src/main/java/Utils/PageUtil.java
@@ -1,59 +1,39 @@
 package Utils;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 
 public class PageUtil implements Pageable {
 
-	private int limit;
-	private int offset;
-	private Sort sort = Sort.unsorted(); //new Sort(Sort.Direction.DESC, "idProdotto");
+	private final int limit;
+	private final int offset;
+	private final Sort sort;
 
-	public PageUtil(int limit, int page, String order, String direction) {
-		super();
-		this.limit = Math.min(limit, 100);
-
-		if(page <= 0) {
-			page = 0;
-		} else {
-			page = limit * (page - 1);
-		}
-		this.offset = page;
-
-		this.sort = direction.toUpperCase()
-				.compareTo("ASC") == 0 ? Sort.by(Sort.Direction.DESC, order) : Sort.by(Sort.Direction.ASC, order);
+	public PageUtil(int limit, int page, String orderColumn, Direction direction) {
+		this(limit, page, Sort.by(direction, orderColumn));
 	}
 
 	public PageUtil(int limit, int page) {
-		if(limit < 1) {
-			throw new IllegalArgumentException("Limit must not be less than one!");
-		}
-		if(page <= 0) {
-			page = 0;
-		} else {
-			page = limit * (page - 1);
-		}
-		this.limit = limit;
-		this.offset = page;
+		this(limit, page, Sort.unsorted());
 	}
 
 	public PageUtil(int limit, int page, Sort sort) {
-		if(limit < 1) {
-			throw new IllegalArgumentException("Limit must not be less than one!");
-		}
-		if(page <= 0) {
-			page = 0;
-		} else {
-			page = limit * (page - 1);
-		}
-		this.limit = limit;
-		this.offset = page;
+
+		this.limit = Math.min(limit, 100);
 		this.sort = sort;
+
+		if(limit < 1)
+			throw new IllegalArgumentException("Limit must not be less than one!");
+
+		offset = (page > 0) ? this.limit * (page - 1) : 0;
+
 	}
 
 	@Override
 	public int getPageNumber() {
-		return offset / limit;
+		return (offset / limit) + 1;
 	}
 
 	@Override
@@ -67,41 +47,33 @@ public class PageUtil implements Pageable {
 	}
 
 	@Override
-	public Sort getSort() {
+	public @NotNull Sort getSort() {
 		return sort;
 	}
 
 	@Override
-	public Pageable next() {
-		// Typecast possible because number of entries cannot be bigger than integer
-		// (primary key is integer)
-		return new PageUtil(getPageSize(), (int) (getOffset() + getPageSize()));
-	}
-
-	public Pageable previous() {
-		// The integers are positive. Subtracting does not let them become bigger than
-		// integer.
-		return hasPrevious() ? new PageUtil(getPageSize(), (int) (getOffset() - getPageSize())) : this;
+	public @NotNull Pageable next() {
+		return new PageUtil(limit, getPageSize() + 1);
 	}
 
 	@Override
-	public Pageable previousOrFirst() {
-		return hasPrevious() ? previous() : first();
+	public @NotNull Pageable previousOrFirst() {
+		return hasPrevious() ? new PageUtil(limit, getPageNumber() - 1) : this;
 	}
 
 	@Override
-	public Pageable first() {
-		return new PageUtil(0, getPageSize());
+	public @NotNull Pageable first() {
+		return new PageUtil(limit, 1);
 	}
 
 	@Override
-	public Pageable withPage(int i) {
-		return null;
+	public @NotNull Pageable withPage(int page) {
+		return new PageUtil(limit, page, sort);
 	}
 
 	@Override
 	public boolean hasPrevious() {
-		return offset > limit;
+		return offset >= limit;
 	}
 
 }
